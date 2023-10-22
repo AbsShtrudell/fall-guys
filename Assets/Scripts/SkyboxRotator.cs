1c356fbe90db57eaf4a2b1f39e293d5d47517bd7
Shtrudell <>
2023-10-22 11:52:20 +0300

    skybox + fog + folders reorganize

---
+++ Assets/Scripts/SkyboxRotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    [SerializeField] private float _rotationPerSecond = 1;
+
+    protected void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * _rotationPerSecond);
+    }
+}
